197e9ed5d88e83d9b3ba5afa934e0933a60a42b7
Tim <timothee.frily@42lausanne.com>
2024-02-19 15:06:18 +0100

    norm ok

--- src/render_utils.c
+++ src/render_utils.c
@@ -6,7 +6,7 @@
 /*   By: tfrily <tfrily@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 16:55:44 by tfrily            #+#    #+#             */
-/*   Updated: 2024/02/19 13:11:41 by tfrily           ###   ########.fr       */
+/*   Updated: 2024/02/19 15:06:03 by tfrily           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,20 @@
  * @param data 
  * @param img A pointer to the player picture.
  */
-void ft_render_player(t_data *data, void *img)
+void	ft_render_player(t_data *data, void *img)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
 	x = 0;
-	while(data->map->map[x])
+	while (data->map->map[x])
 	{
 		y = 0;
 		while (data->map->map[x][y])
 		{
 			if (data->map->map[x][y] == 'P')
-				mlx_put_image_to_window(data->mlx, data->window,img, y*40-5,x*40-7);
+				mlx_put_image_to_window(data->mlx,
+					data->window, img, y * 40 - 5, x * 40 - 7);
 			y++;
 		}
 		x++;

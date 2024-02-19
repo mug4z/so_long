bbf17a46d16dd237af16abfa40a70333c59446dd
Tim <timothee.frily@42lausanne.com>
2024-02-19 13:42:27 +0100

    norm ok

--- src/checker_utils.c
+++ src/checker_utils.c
@@ -6,31 +6,32 @@
 /*   By: tfrily <tfrily@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 15:04:41 by tfrily            #+#    #+#             */
-/*   Updated: 2024/02/19 10:35:57 by tfrily           ###   ########.fr       */
+/*   Updated: 2024/02/19 13:42:04 by tfrily           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
 /**
- * @brief  Check that only correct elements are on the map and the map is not empty or file start with \n 
+ * @brief  Check that only correct elements are on the 
+ * 			map and the map is not empty or file start with \n 
  * 
  * @param data Struct that contain many information about the game
  */
-void ft_chk_elements(t_data *data)
+void	ft_chk_elements(t_data *data)
 {
-	char *res;
-	
+	char	*res;
+
 	res = get_next_line(data->map_fd);
-	if(res == NULL)
+	if (res == NULL)
 	{
 		ft_err_empty_map(data);
 		exit(1);
 	}
 	ft_remove_new_line(res);
-	while(res != NULL)
+	while (res != NULL)
 	{
-		if(res[0] == '\0')
+		if (res[0] == '\0')
 		{
 			ft_err_invalid_map(data);
 			exit(1);
@@ -47,10 +48,10 @@ void ft_chk_elements(t_data *data)
  * 
  * @param data Struct that contain many information about the game
  */
-void ft_chk_rect(t_data *data)
+void	ft_chk_rect(t_data *data)
 {
-	int col;
-	int col2;
+	int	col;
+	int	col2;
 	int	x;
 
 	x = 0;
@@ -72,15 +73,15 @@ void ft_chk_rect(t_data *data)
  * 
  * @param data Struct that contain many information about the game
  */
-void ft_chk_surround_wall(t_data *data)
+void	ft_chk_surround_wall(t_data *data)
 {
-	int 	x;
+	int	x;
 
 	x = 0;
-	while(data->map->map[0][x] && data->map->map[data->map->lines - 1][x])
+	while (data->map->map[0][x] && data->map->map[data->map->lines - 1][x])
 	{
-		if (data->map->map[0][x] != '1' || 
-			data->map->map[data->map->lines - 1][x] != '1')
+		if (data->map->map[0][x] != '1' ||
+				data->map->map[data->map->lines - 1][x] != '1')
 		{
 			ft_err_surrounded_wall(data);
 			exit(1);
@@ -88,10 +89,10 @@ void ft_chk_surround_wall(t_data *data)
 		x++;
 	}
 	x = 1;
-	while(data->map->map[x])
+	while (data->map->map[x])
 	{
-		if(data->map->map[x][0] != '1' || 
-		   data->map->map[x][ft_strlen(data->map->map[x]) - 1] != '1')
+		if (data->map->map[x][0] != '1' ||
+				data->map->map[x][ft_strlen(data->map->map[x]) - 1] != '1')
 		{
 			ft_err_surrounded_wall(data);
 			exit(1);
@@ -105,19 +106,19 @@ void ft_chk_surround_wall(t_data *data)
  * 
  * @param data Struct that contain many information about the game
  */
-void ft_chk_min_elements(t_data *data)
+void	ft_chk_min_elements(t_data *data)
 {
 	if (ft_count_elements(data->map->map, 'C') < 1)
 	{
 		ft_err_not_engough_item(data);
-		exit(1);			
+		exit(1);
 	}
-	if(ft_count_elements(data->map->map, 'E') != 1)
+	if (ft_count_elements(data->map->map, 'E') != 1)
 	{
 		ft_err_exit(data);
 		exit(1);
 	}
-	if(ft_count_elements(data->map->map, 'P') != 1)
+	if (ft_count_elements(data->map->map, 'P') != 1)
 	{
 		ft_err_player(data);
 		exit(1);
@@ -125,18 +126,19 @@ void ft_chk_min_elements(t_data *data)
 }
 
 /**
- * @brief Check if the map is possible using floodfill algorithme. Begin at the player position.
+ * @brief Check if the map is possible using floodfill algorithme. 
+ * 		Begin at the player position.
  * 
  * @param p_position Player positions in the map.
  * @param data Struct that contain many information about the game
  */
-void ft_chk_map_validity(int p_position[2],t_data *data)
+void	ft_chk_map_validity(int p_position[2], t_data *data)
 {
-	ft_floodfill(p_position[0],p_position[1],data->map);
-	if(ft_count_elements(data->map->map, 'C') != 0 || ft_count_elements(data->map->map,'E') != 0)
+	ft_floodfill(p_position[0], p_position[1], data->map);
+	if (ft_count_elements(data->map->map, 'C') != 0
+		|| ft_count_elements(data->map->map, 'E') != 0)
 	{
-		ft_print_err(data,": is impossible to complete");
+		ft_print_err(data, ": is impossible to complete");
 		exit(1);
 	}	
 }
-

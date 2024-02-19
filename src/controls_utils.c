13c016fc004fd42c86a738aca4d023c2087587dd
Tim <timothee.frily@42lausanne.com>
2024-02-19 14:35:52 +0100

    norm ok

--- src/controls_utils.c
+++ src/controls_utils.c
@@ -6,7 +6,7 @@
 /*   By: tfrily <tfrily@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 16:34:17 by tfrily            #+#    #+#             */
-/*   Updated: 2024/02/19 13:22:12 by tfrily           ###   ########.fr       */
+/*   Updated: 2024/02/19 14:35:37 by tfrily           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,23 @@
  * @param nb Equal to 1 or -1. It moves player or exit on the map.
  * @param direction 1 or 0. to separate the up left and down right.
  */
-void ft_move_from_exit(t_data *data, int nb, int direction)
+void	ft_move_from_exit(t_data *data, int nb, int direction)
 {
-    if (direction == 1)
-    {
-        data->map->map[data->p_x_pos][data->p_y_pos] = 'E';
-        data->p_x_pos += nb;
-        data->map->map[data->p_x_pos][data->p_y_pos] = 'P';
-        data->step_count++;        
-    }
-    else
-    {
-        data->map->map[data->p_x_pos][data->p_y_pos] = 'E';
-        data->p_y_pos += nb;
-        data->map->map[data->p_x_pos][data->p_y_pos] = 'P';
-        data->step_count++;
-    }
-    ft_render_map(data,ft_itoa(data->step_count));   
+	if (direction == 1)
+	{
+		data->map->map[data->p_x_pos][data->p_y_pos] = 'E';
+		data->p_x_pos += nb;
+		data->map->map[data->p_x_pos][data->p_y_pos] = 'P';
+		data->step_count++;
+	}
+	else
+	{
+		data->map->map[data->p_x_pos][data->p_y_pos] = 'E';
+		data->p_y_pos += nb;
+		data->map->map[data->p_x_pos][data->p_y_pos] = 'P';
+		data->step_count++;
+	}
+	ft_render_map(data, ft_itoa(data->step_count));
 }
 
 /**
@@ -45,29 +45,28 @@ void ft_move_from_exit(t_data *data, int nb, int direction)
  * @param nb Equal to 1 or -1. It moves player or exit on the map.
  * @param direction 1 or 0. to separate the up left and down right.
  */
-void ft_move(t_data *data, int nb, int direction)
+void	ft_move(t_data *data, int nb, int direction)
 {
-    if (data->map->map[data->p_x_pos][data->p_y_pos] == 
-            data->map->map[data->e_x_pos][data->e_y_pos])
-    {
-        ft_move_from_exit(data,nb,direction); 
-        return;
-    }
-    if (direction == 1)
-    {
-        data->map->map[data->p_x_pos][data->p_y_pos] = '0';
-        data->p_x_pos += nb;
-        data->map->map[data->p_x_pos][data->p_y_pos] = 'P';
-        data->step_count++;
-    }
-    else
-    {
-        data->map->map[data->p_x_pos][data->p_y_pos] = '0';
-        data->p_y_pos += nb;
-        data->map->map[data->p_x_pos][data->p_y_pos] = 'P';
-        data->step_count++;
-    }
-    
-    ft_render_map(data,ft_itoa(data->step_count));
+	if (data->map->map[data->p_x_pos][data->p_y_pos] ==
+			data->map->map[data->e_x_pos][data->e_y_pos])
+	{
+		ft_move_from_exit(data, nb, direction);
+		return ;
+	}
+	if (direction == 1)
+	{
+		data->map->map[data->p_x_pos][data->p_y_pos] = '0';
+		data->p_x_pos += nb;
+		data->map->map[data->p_x_pos][data->p_y_pos] = 'P';
+		data->step_count++;
+	}
+	else
+	{
+		data->map->map[data->p_x_pos][data->p_y_pos] = '0';
+		data->p_y_pos += nb;
+		data->map->map[data->p_x_pos][data->p_y_pos] = 'P';
+		data->step_count++;
+	}
+	ft_render_map(data, ft_itoa(data->step_count));
 }
 // ft_printf("Movement : %d",data->step_count):

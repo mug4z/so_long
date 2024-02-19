992a9736c1fa6dc80db2b2f774ad2e2fe90105aa
Tim <timothee.frily@42lausanne.com>
2024-02-19 14:38:02 +0100

    norm ok

--- src/error_utils1.c
+++ src/error_utils1.c
@@ -6,13 +6,13 @@
 /*   By: tfrily <tfrily@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:40:12 by tfrily            #+#    #+#             */
-/*   Updated: 2024/02/16 15:25:03 by tfrily           ###   ########.fr       */
+/*   Updated: 2024/02/19 14:37:36 by tfrily           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-void ft_err_player(t_data *data)
+void	ft_err_player(t_data *data)
 {
 	ft_putstr_fd("ERROR\n", 2);
 	ft_putstr_fd(data->map_name, 2);
@@ -22,7 +22,7 @@ void ft_err_player(t_data *data)
 	ft_clean((char *)data);
 }
 
-void ft_print_err(t_data *data, char *msg)
+void	ft_print_err(t_data *data, char *msg)
 {
 	ft_putstr_fd("ERROR\n", 2);
 	ft_putstr_fd(data->map_name, 2);
@@ -33,18 +33,18 @@ void ft_print_err(t_data *data, char *msg)
 	ft_clean((char *)data);
 }
 
-void ft_print_error(char *what, char *msg,t_data *data)
+void	ft_print_error(char *what, char *msg, t_data *data)
 {
 	ft_putstr_fd("ERROR\n", 2);
 	ft_putstr_fd(what, 2);
 	ft_putstr_fd(msg, 2);
 	if (data->map != NULL)
 		ft_clean2dtable(data->map->map);
-	mlx_destroy_image(data->mlx,data->img_grass);
-	mlx_destroy_image(data->mlx,data->img_tree);
-	mlx_destroy_image(data->mlx,data->img_collec);
-	mlx_destroy_image(data->mlx,data->img_exit);
-	mlx_destroy_image(data->mlx,data->img_character);
+	mlx_destroy_image(data->mlx, data->img_grass);
+	mlx_destroy_image(data->mlx, data->img_tree);
+	mlx_destroy_image(data->mlx, data->img_collec);
+	mlx_destroy_image(data->mlx, data->img_exit);
+	mlx_destroy_image(data->mlx, data->img_character);
 	ft_clean((char *) data->map);
 	ft_clean((char *)data);
 }

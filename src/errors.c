9951553c620718563bd17161a002a6cfcff1308f
Tim <timothee.frily@42lausanne.com>
2024-02-19 14:40:19 +0100

    norm ok

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: tfrily <tfrily@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 17:59:21 by tfrily            #+#    #+#             */
-/*   Updated: 2024/02/12 11:59:30 by tfrily           ###   ########.fr       */
+/*   Updated: 2024/02/19 14:39:54 by tfrily           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,30 @@
 
 // Functions to print error.
 
-void ft_err_ext(t_data *data)
+void	ft_err_ext(t_data *data)
 {	
-	ft_putstr_fd("Error\n",2);
-	ft_putstr_fd(data->map_name,2);
-	ft_putstr_fd(": wrong extension",2);
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd(data->map_name, 2);
+	ft_putstr_fd(": wrong extension", 2);
 	ft_clean((char *)data);
 }
 
-void ft_err_filevalid(t_data *data)
+void	ft_err_filevalid(t_data *data)
 {
-	ft_putstr_fd("ERROR\n",2);
+	ft_putstr_fd("ERROR\n", 2);
 	perror(data->map_name);
 	ft_clean((char *)data);
 }
 
-void ft_err_elements(char c, char *line)
+void	ft_err_elements(char c, char *line)
 {
-	ft_putstr_fd("Error\n",2);
-	ft_putchar_fd(c,2);
-	ft_putstr_fd(": wrong elements",2);
+	ft_putstr_fd("Error\n", 2);
+	ft_putchar_fd(c, 2);
+	ft_putstr_fd(": wrong elements", 2);
 	ft_clean(line);
 }
 
-void ft_err_directory(t_data *data)
+void	ft_err_directory(t_data *data)
 {
 	ft_putstr_fd("ERROR\n", 2);
 	ft_putstr_fd(data->map_name, 2);
@@ -45,7 +45,7 @@ void ft_err_directory(t_data *data)
 	ft_clean((char *)data);
 }
 
-void ft_err_empty_map(t_data *data)
+void	ft_err_empty_map(t_data *data)
 {
 	ft_putstr_fd("ERROR\n", 2);
 	ft_putstr_fd(data->map_name, 2);
